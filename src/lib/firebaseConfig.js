0c1ac640c676adf7f403fba423e603c18e988428
Henrique Derosa <henriquederosa@Henriques-MacBook-Pro.local>
2021-03-26 17:12:24 -0300

    first commit

---
+++ src/lib/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyAxMAwZqANy6Jvp4S9ne7Dx6m8GXOYlVJg",
+  authDomain: "pius-mobile-app.firebaseapp.com",
+  projectId: "pius-mobile-app",
+  storageBucket: "pius-mobile-app.appspot.com",
+  messagingSenderId: "996910153402",
+  appId: "1:996910153402:web:49c89223fa5c5b89bdcc83",
+  measurementId: "G-1R5VLKL6XR",
+};
